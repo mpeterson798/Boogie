adbfe1a0df77b134f8fff6b802249c2baa412bec
Matthew Peterson <matthew251125@icloud.com>
2025-09-14 15:08:30 -0500

    Initial Commit

---
+++ Boogie/BoogieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogieApp.swift
+//  Boogie
+//
+//  Created by Matthew Peterson on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BoogieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
