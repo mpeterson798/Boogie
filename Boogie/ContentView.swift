6992d227503762e4d52a4c42fcb2b1fd7c8fc623
Matthew Peterson <matthew251125@icloud.com>
2025-09-14 20:13:35 -0500

    1st push

--- Boogie/ContentView.swift
+++ Boogie/ContentView.swift
@@ -6,14 +6,74 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var imageList = ["globe", "wind", "flame"]
+    @State private var image = ""
+    @State private var image1 = ""
+    @State private var image2 = ""
+    @State private var image3 = ""
+    @State private var pressCount = 1
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer ()
+            
+            Text("Boogie Wonderland!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            
+            HStack {
+                
+                Image(systemName: image1)
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.teal)
+                    .frame(width: 120, height: 120)
+                
+                Image(systemName: image2)
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                    .frame(width: 120, height: 120)
+
+                Image(systemName: image3)
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.red)
+                    .frame(width: 120, height: 120)
+
+            }
+            
+            Spacer()
+            
+            Button("Funk!") {
+                
+                
+                if pressCount == 1 {
+                    image1 = "globe"
+                } else if pressCount == 2 {
+                    image1 = "globe"
+                    image2 = "wind"
+                } else if pressCount == 3 {
+                    image1 = "globe"
+                    image2 = "wind"
+                    image3 = "flame"
+                } else {
+                    image1 = "globe"
+                    image2 = "wind"
+                    image3 = "flame"
+                    
+                }
+                
+                pressCount += 1
+            }
+            .buttonStyle(.bordered)
         }
         .padding()
     }
